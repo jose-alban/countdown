ced12b5e9bde010f0ffd0a3ffc1493d941a6cdd9
lisbeth balabarca <lisbethbalabarca@gmail.com>
2016-07-09 15:50:58 -0500

    php cambios

--- mail.php
+++ mail.php
@@ -1,7 +1,8 @@
 <?php
-	$destino="lisbethbalabarca@gmail.com"
-	$correo= $_POST["email"]
+	$destino = "lisbethbalabarca@gmail.com";
+	$subject = "Tourstap Subscription - Counter Page";
+	$correo = $_POST["email"]
+	$contenido = "Hola este es mi correo " . $correo . ", solicito que me puedan enviar la información.";
 
-	$contenido= "Hola este es mi correo" . $correo . "solicito que me puedan enviar la información.";
-	mail($destino , $contenido);
+	mail($destino, $subject, $contenido);
 ?>
