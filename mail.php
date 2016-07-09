057ef3fecd871a9fa284a94138e930df48515bb4
Jose Alban <jose_alban@outlook.com>
2016-07-09 14:49:14 -0700

    Update mail.php for bluehost
    This example works with Bluehost.

--- mail.php
+++ mail.php
@@ -1,8 +1,22 @@
 <?php
-	$destino = "lisbethbalabarca@gmail.com";
-	$subject = "Tourstap Subscription - Counter Page";
-	$correo = $_POST["email"]
-	$contenido = "Hola este es mi correo " . $correo . ", solicito que me puedan enviar la información.";
-
-	mail($destino, $subject, $contenido);
+	//Bluehost Example
+	$emailto = 'lisbethbalabarca@gmail.com';
+	$toname = 'Tourstap Susbcription';
+	$emailfrom = 'subscriptions@tourstap.com';
+	$fromname = 'Tourstap Susbcription';
+	$subject = 'Subscriber from our counter page';
+	$messagebody = 'Hola este es mi correo ' . $emailfrom . ', solicito que me puedan enviar la información.';
+	$headers = 
+		'Return-Path: ' . $emailfrom . "\r\n" . 
+		'From: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" . 
+		'X-Priority: 3' . "\r\n" . 
+		'X-Mailer: PHP ' . phpversion() .  "\r\n" . 
+		'Reply-To: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" .
+		'MIME-Version: 1.0' . "\r\n" . 
+		'Content-Transfer-Encoding: 8bit' . "\r\n" . 
+		'Content-Type: text/plain; charset=UTF-8' . "\r\n";
+	$params = '-f ' . $emailfrom;
+	$test = mail($emailto, $subject, $messagebody, $headers, $params);
+	// $test should be TRUE if the mail function is called correctly
+	header("Location: /index.html");
 ?>
